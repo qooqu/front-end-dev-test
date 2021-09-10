e1004bf2eea8e251352f5e03d01427c22af5794e
qooqu <qooqu@users.noreply.github.com>
2021-09-10 15:02:11 -0700

    first

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,59 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-	entry: './src/app.js',
+    entry: "./src/app.js",
 
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js'
-	},
-	watch: true,
-	devServer: {
-		inline: true
-	},
-	//Add sass-loader
-	module: {
-		rules: [
-      {
-        test: /\.html$/,
-        loader: "html-loader"
-      },
-			{
-				test: /\.(scss)$/,
-				use: [{
-					loader: 'style-loader'
-				}, {
-					loader: 'css-loader'
-				}, {
-					loader: 'sass-loader'
-				}, {
-					loader: 'postcss-loader',
-					options: {
-						plugins: function () {
-							return [ require('autoprefixer')]
-						}
-					}
-				}]
-			}
-		]
-	}
-}
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+    },
+    watch: true,
+    devServer: {
+        inline: true,
+        host: "0.0.0.0",
+        port: 3000,
+    },
+    //Add sass-loader
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(scss)$/,
+                use: [
+                    {
+                        loader: "style-loader",
+                    },
+                    {
+                        loader: "css-loader",
+                    },
+                    {
+                        loader: "sass-loader",
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            plugins: function () {
+                                return [require("autoprefixer")];
+                            },
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                loader: "file-loader",
+                options: {
+                    name: "[name].[ext]",
+                },
+            },
+            {
+                test: /\.svg$/,
+                loader: "svg-inline-loader",
+            },
+        ],
+    },
+};
